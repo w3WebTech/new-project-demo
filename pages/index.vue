f8a7a01605379236f9b7bbf6d0b426e0f77e48ba
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-11-04 14:36:11 +0530

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,14 @@
 
 <template>
-  <OnboardingSlider />
+  <OnBoardingSlider />
 </template>
 
 <script lang="ts">
-import OnBoardingSlider from '@/components/OnBoardingSlider.vue';
+import OnBoardingSlider from '@/components/OnBoardingSlider.vue'; // Use alias @ to point to the components directory
 
 export default {
   components: {
     OnBoardingSlider
   }
 }
-
 </script>
