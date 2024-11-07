68e468448808f0706ba9b92f7c763f702f8e6012
w3WebTech <164163622+w3WebTech@users.noreply.github.com>
2024-11-07 11:26:53 +0530

    fix

--- pages/FifthStep.vue
+++ pages/FifthStep.vue
@@ -423,9 +423,9 @@
             
           </div>
         </div>
-           <div class="relative" v-else >
-            <div class="m-auto w-full border-10 border-gray-800">
-              <video ref="videoElement" class="w-full " autoplay></video>
+           <div class="relative h-screen " v-else >
+            <div class="m-auto w-full border-10 border-gray-800 bg-red-500 h-[300px]" >
+              <video ref="videoElement" class="w-full h-full" autoplay></video>
             </div>
             <canvas ref="canvas" class="hidden"></canvas>
 
